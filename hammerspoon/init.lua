3b36dcfcd467b9c58080a06054d03b9fb9d6735d
JeeBak Kim <jeebak.kim@gmail.com>
2017-12-03 13:22:35 -0800

    Misc updates to hammerspoon/init.lua
    - Add local fontName = 'SauceCodePro Nerd Font'
    - Add local hyper = {'shift', 'ctrl', 'alt', 'cmd'}
    - Change reload config to hyper-q
    - Add require('hs.expose')
    - Add require('hs.hints')
    - Add require('hs.tabs')
    - Add require('hs.window.highlight')
    - Add require('hs.window.switcher')
    - Add require('keyboard.grids')

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,7 +1,9 @@
+local fontName = 'SauceCodePro Nerd Font'
+local hyper = {'shift', 'ctrl', 'alt', 'cmd'}
 local log = hs.logger.new('init.lua', 'debug')
 
--- Use Control+` to reload Hammerspoon config
-hs.hotkey.bind({'ctrl'}, '`', nil, function()
+-- Use Hyper+q to reload Hammerspoon config
+hs.hotkey.bind(hyper, 'q', nil, function()
   hs.reload()
 end)
 
@@ -31,7 +33,56 @@ enableHotkeyForWindowsMatchingFilter = function(windowFilter, hotkey)
   end)
 end
 
+-------------------------------------------------------------------------------
+-- /Applications/Hammerspoon.app/Contents/Resources/extensions/hs/expose
+local expose = require('hs.expose')
+expose.ui.fontName = fontName
+expose.ui.maxHintLetters = 1 -- if necessary, hints longer than this will be disambiguated with digits
+expose.ui.fitWindowsMaxIterations = 10 -- lower is faster, but higher chance of overlapping thumbnails
+local expose_app = expose.new(nil,{onlyActiveApplication=true}) -- show windows for the current application
+local expose_space = expose.new(nil,{includeOtherSpaces=false}) -- only windows in the current Mission Control Space
+hs.hotkey.bind(hyper, 'a', function()expose_app:toggleShow()end)
+hs.hotkey.bind(hyper, 's', function()expose_space:toggleShow()end)
+-------------------------------------------------------------------------------
+-- /Applications/Hammerspoon.app/Contents/Resources/extensions/hs/hints
+local hints = require('hs.hints')
+hints.fontName = fontName
+hints.fontSize = 24
+hs.hotkey.bind(hyper, 'f', hints.windowHints)
+-------------------------------------------------------------------------------
+-- /Applications/Hammerspoon.app/Contents/Resources/extensions/hs/mjomatic
+-- local mjomatic = require('hs.mjomatic')
+-------------------------------------------------------------------------------
+-- /Applications/Hammerspoon.app/Contents/Resources/extensions/hs/tabs
+local tabs = require('hs.tabs')
+tabs.enableForApp('Code')
+-------------------------------------------------------------------------------
+-- /Applications/Hammerspoon.app/Contents/Resources/extensions/hs/window/highlight.lua
+local hl = require('hs.window.highlight')
+hl.ui.isolateColor = {0,0,0,0.85}       -- overlay color for isolate mode
+hl.ui.overlayColor = {0.2,0.05,0,0.30}  -- overlay color
+-- hl.ui.flashDuration = 0.3 -- too distracting
+-- Toggle Isolate
+hs.hotkey.bind(hyper, 'i', hl.toggleIsolate)
+-- Toggle Overlay
+hs.hotkey.bind(hyper, 'o', function() hl.ui.overlay = not hl.ui.overlay end)
+hl.start()
+-------------------------------------------------------------------------------
+-- /Applications/Hammerspoon.app/Contents/Resources/extensions/hs/window/switcher.lua
+require('hs.window.switcher')
+hs.window.filter.ignoreAlways['Hyper'] = true
+hs.window.filter.ignoreAlways['Terminal'] = true
+hs.window.filter.ignoreAlways['iTerm2'] = true
+hs.window.filter.ignoreAlways['kitty'] = true
+hs.window.switcher.ui.fontName = fontName
+hs.window.switcher.ui.highlightColor = {0.8,0.8,1,0.5} -- highlight color for the selected window
+local switcher = hs.window.switcher.new(hs.window.filter.new():setCurrentSpace(true):setDefaultFilter{}) -- include minimized/hidden windows, current Space only
+hs.hotkey.bind('alt', 'tab', function()switcher:next()end)
+hs.hotkey.bind('alt-shift', 'tab', function()switcher:previous()end)
+-------------------------------------------------------------------------------
 -- require('keyboard.control-escape')
+-- require('keyboard.delete-words')
+require('keyboard.grids')
 -- require('keyboard.hyper')
 -- require('keyboard.markdown')
 -- require('keyboard.microphone')
